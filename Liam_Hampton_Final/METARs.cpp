212e4ac03d3c386cf1c45d8dba88d20fac94ae45
LiamHampton <wiha0864@colorado.edu>
2015-04-22 08:25:19 -0600

    Shell

---
+++ Liam_Hampton_Final/METARs.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+#include <METARs.h>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <queue>
+
+using namespace std;
---
+++ Liam_Hampton_Final/main.cpp
@@ -0,0 +1,19 @@
+//Includes Elmendorf Boulder Seattle San Francisco JFK Las Vegas, LAX, Amsterdam, Denver.
+//http://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=csv
+//&stationString=PAED KBOL KSEA KSFO KJFK KLAS KLAX EHAM ,KDEN&hoursBeforeNow=12
+
+//Liam Hampton  Final Project.
+
+#include <iostream>
+#include <vector>
+#include <METARs.h>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+void main (){
+
+}
